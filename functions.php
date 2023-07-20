b0e69b380861924c19cca859972cccdefe501dcd
Nathan Rollins <nathan.rollins@asu.edu>
2023-07-20 16:17:25 -0700

    Add thumbnail support for Project CPTs

--- functions.php
+++ functions.php
@@ -14,3 +14,7 @@ require get_stylesheet_directory() . '/inc/enqueue-assets.php';
 require get_stylesheet_directory() . '/inc/custom-post-types.php';
 require get_stylesheet_directory() . '/inc/acf-register.php';
 
+/**
+ * Add Theme Support for Project Thumbnails
+ * */
+add_theme_support( 'post-thumbnails', array( 'project' ) );
