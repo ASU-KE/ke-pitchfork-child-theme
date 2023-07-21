98b4df169da68e95597db0970dd92afbf9335bfd
Nathan Rollins <nathan.rollins@asu.edu>
2023-07-21 11:54:10 -0700

    Update enqueuing logic

--- inc/enqueue-assets.php
+++ inc/enqueue-assets.php
@@ -11,27 +11,27 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 
-// Enqueue child scripts and styles.
-function pitchfork_child_scripts() {
+// Enqueue child theme scripts and styles.
+function ke_pitchfork_enqueue_frontend() {
 
-    $cssFilePath = glob( get_template_directory() . '/css/build/child-theme.min.*.css' );
-    $cssFileURI = get_template_directory_uri() . '/css/build/' . basename($cssFilePath[0]);
+    $cssFilePath = glob( get_stylesheet_directory() . '/css/build/childTheme.min.*.css' );
+    $cssFileURI = get_stylesheet_directory_uri() . '/css/build/' . basename($cssFilePath[0]);
     wp_enqueue_style( 'ke-pitchfork-styles', $cssFileURI );
 
-    $jsFilePath = glob( get_template_directory() . '/js/build/child-theme.min.*.js' );
-    $jsFileURI = get_template_directory_uri() . '/js/build/' . basename($jsFilePath[0]);
+    $jsFilePath = glob( get_stylesheet_directory() . '/js/build/childTheme.min.*.js' );
+    $jsFileURI = get_stylesheet_directory_uri() . '/js/build/' . basename($jsFilePath[0]);
     wp_enqueue_script( 'ke-pitchfork-scripts', $jsFileURI , null , null , true );
 
 }
-add_action( 'wp_enqueue_scripts', 'pitchfork_child_scripts' );
+add_action( 'wp_enqueue_scripts', 'ke_pitchfork_enqueue_frontend' );
 
 
-// Enqueue to the admin. Block editor styles only.
-function pitchfork_child_enqueue_editor_scripts() {
+// Enqueue the Block Editor styles only.
+function ke_pitchfork_enqueue_editor_styles() {
 
-    $cssFilePath = glob( get_template_directory() . '/css/build/block-editor.min.*.css' );
-    $cssFileURI = get_template_directory_uri() . '/css/build/' . basename($cssFilePath[0]);
+    $cssFilePath = glob( get_stylesheet_directory() . '/css/build/blockEditor.min.*.css' );
+    $cssFileURI = get_stylesheet_directory_uri() . '/css/build/' . basename($cssFilePath[0]);
     wp_enqueue_style( 'ke-pitchfork-blockeditor-styles', $cssFileURI );
 
 }
-add_action('enqueue_block_editor_assets', 'pitchfork_child_enqueue_editor_scripts');
+add_action('enqueue_block_editor_assets', 'ke_pitchfork_enqueue_editor_styles');
