b6ced98830775fa5d929b4fc7770e1bacdda548f
Nathan Rollins <nathan.rollins@asu.edu>
2023-08-03 13:47:00 -0700

    Add full-page patterns for KE Sitelets

---
+++ patterns/ke-sitelet-home.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Title: KE Sitelet Home
+ * Slug: ke-sitelet-home
+ * Description: The Home page layout for KE Sitelets.
+ * Categories: ke-sitelet
+ * Keywords: full-width
+ * Viewport Width: 1920
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+
+?>
+<!-- wp:acf/hero {"name":"acf/hero","data":{"uds_hero_size":"uds-hero-md","_uds_hero_size":"field_611425a016b0e","uds_hero_image":34,"_uds_hero_image":"field_6114269916b0f"},"mode":"preview"} -->
+<!-- wp:acf/subtitle {"name":"acf/subtitle","data":{"uds_subtitle_text":"","_uds_subtitle_text":"field_6250abe44b020","uds_subtitle_highlight_color":"none","_uds_subtitle_highlight_color":"field_6250ac5e4b021"},"mode":"preview"} /-->
+
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><span class="highlight-gold">Lorem Ipsum</span></h1>
+<!-- /wp:heading -->
+
+<!-- wp:group {"className":"content"} -->
+<div class="wp-block-group content"><!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color"></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+<!-- /wp:acf/hero -->
+
+<!-- wp:acf/content-image-overlap {"name":"acf/content-image-overlap","data":{"uds_image_overlap_background":37,"_uds_image_overlap_background":"field_602314ea0c7df","uds_image_overlap_orientation":"right","_uds_image_overlap_orientation":"field_6023169c15df8"},"mode":"preview"} -->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">About us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button /--></div>
+<!-- /wp:buttons -->
+<!-- /wp:acf/content-image-overlap -->
+
+<!-- wp:spacer {"height":"96px"} -->
+<div style="height:96px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|uds-size-4","bottom":"var:preset|spacing|uds-size-2"}}}} -->
+<h2 class="wp-block-heading" style="margin-top:var(--wp--preset--spacing--uds-size-4);margin-bottom:var(--wp--preset--spacing--uds-size-2)">Focus areas</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>The LEI’s work comprises four areas:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:acf/uds-card {"name":"acf/uds-card","data":{"title":"Lorem Ipsum","_title":"field_602f0c315bfc0","body_text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","_body_text":"field_602f0c412559a","card_style":"basic","_card_style":"field_602f0b39b4c29","header_style":"image","_header_style":"field_602f0b5660bc4","image":40,"_image":"field_602f0bafbc593","card_orientation":"vertical","_card_orientation":"field_602f0c2622823","buttons":"","_buttons":"field_602f0cc0abbc7","links":"","_links":"field_602f0e97702ae","tags":"","_tags":"field_602f0f084ce05"},"mode":"preview"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:acf/uds-card {"name":"acf/uds-card","data":{"title":"Lorem Ipsum","_title":"field_602f0c315bfc0","body_text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","_body_text":"field_602f0c412559a","card_style":"basic","_card_style":"field_602f0b39b4c29","header_style":"image","_header_style":"field_602f0b5660bc4","image":40,"_image":"field_602f0bafbc593","card_orientation":"vertical","_card_orientation":"field_602f0c2622823","buttons":"","_buttons":"field_602f0cc0abbc7","links":"","_links":"field_602f0e97702ae","tags":"","_tags":"field_602f0f084ce05"},"mode":"preview"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:acf/uds-card {"name":"acf/uds-card","data":{"title":"Lorem Ipsum","_title":"field_602f0c315bfc0","body_text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","_body_text":"field_602f0c412559a","card_style":"basic","_card_style":"field_602f0b39b4c29","header_style":"image","_header_style":"field_602f0b5660bc4","image":40,"_image":"field_602f0bafbc593","card_orientation":"vertical","_card_orientation":"field_602f0c2622823","buttons":"","_buttons":"field_602f0cc0abbc7","links":"","_links":"field_602f0e97702ae","tags":"","_tags":"field_602f0f084ce05"},"mode":"preview"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:acf/uds-card {"name":"acf/uds-card","data":{"title":"Lorem Ipsum","_title":"field_602f0c315bfc0","body_text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","_body_text":"field_602f0c412559a","card_style":"basic","_card_style":"field_602f0b39b4c29","header_style":"image","_header_style":"field_602f0b5660bc4","image":40,"_image":"field_602f0bafbc593","card_orientation":"vertical","_card_orientation":"field_602f0c2622823","buttons":"","_buttons":"field_602f0cc0abbc7","links":"","_links":"field_602f0e97702ae","tags":"","_tags":"field_602f0f084ce05"},"mode":"preview"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
---
+++ patterns/ke-sitelet-projects.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Title: KE Sitelet Projects
+ * Slug: ke-sitelet-projects
+ * Description: The Projects page layout for KE Sitelets.
+ * Categories: ke-sitelet
+ * Keywords: full-width
+ * Viewport Width: 1920
+ * Block Types:
+ * Post Types:
+ * Inserter: true
+ */
+
+?>
+<!-- wp:acf/hero {"name":"acf/hero","data":{"uds_hero_size":"uds-hero-md","_uds_hero_size":"field_611425a016b0e","uds_hero_image":34,"_uds_hero_image":"field_6114269916b0f"},"mode":"preview"} -->
+<!-- wp:acf/subtitle {"name":"acf/subtitle","data":{"uds_subtitle_text":"","_uds_subtitle_text":"field_6250abe44b020","uds_subtitle_highlight_color":"none","_uds_subtitle_highlight_color":"field_6250ac5e4b021"},"mode":"preview"} /-->
+
+<!-- wp:heading {"level":1} -->
+<h1 class="wp-block-heading"><span class="highlight-gold">Projects</span></h1>
+<!-- /wp:heading -->
+
+<!-- wp:group {"className":"content"} -->
+<div class="wp-block-group content"><!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color"></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+<!-- /wp:acf/hero -->
+
+<!-- wp:acf/content-image-overlap {"name":"acf/content-image-overlap","data":{"uds_image_overlap_background":37,"_uds_image_overlap_background":"field_602314ea0c7df","uds_image_overlap_orientation":"right","_uds_image_overlap_orientation":"field_6023169c15df8"},"mode":"preview"} -->
+<!-- wp:heading -->
+<h2 class="wp-block-heading">The Center's projects</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button /--></div>
+<!-- /wp:buttons -->
+<!-- /wp:acf/content-image-overlap -->
+
+<!-- wp:spacer {"height":"96px"} -->
+<div style="height:96px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading -->
+<h2 class="wp-block-heading">Lorem ipsum dolor sit amet</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:spacer {"height":"96px"} -->
+<div style="height:96px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:acf/background-section {"name":"acf/background-section","data":{"uds_background_section_choice":"upload","_uds_background_section_choice":"field_603819ea114a9","uds_background_section_upload_url":58,"_uds_background_section_upload_url":"field_60381d8c3ba87","uds_background_image_repeat":"repeat","_uds_background_image_repeat":"field_606e31f365933","uds_background_image_position":"center","_uds_background_image_position":"field_606e340a65934","uds_background_image_size":"auto","_uds_background_image_size":"field_607f2bc905fd6"},"mode":"preview"} -->
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:acf/uds-card {"name":"acf/uds-card","data":{"title":"Lorem Ipsum","_title":"field_602f0c315bfc0","body_text":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.","_body_text":"field_602f0c412559a","card_style":"basic","_card_style":"field_602f0b39b4c29","header_style":"none","_header_style":"field_602f0b5660bc4","card_orientation":"vertical","_card_orientation":"field_602f0c2622823","buttons":"","_buttons":"field_602f0cc0abbc7","links":"","_links":"field_602f0e97702ae","tags":"","_tags":"field_602f0f084ce05"},"mode":"edit"} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+<!-- /wp:acf/background-section -->
