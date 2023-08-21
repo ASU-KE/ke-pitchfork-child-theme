ba9d0a50c6065c2582b6bf2fbf4727ece4705dd4
Nathan Rollins <nathan.rollins@asu.edu>
2023-08-21 15:02:07 -0700

    Drop GFL patterns category

--- functions.php
+++ functions.php
@@ -17,16 +17,3 @@ require get_stylesheet_directory() . '/inc/acf-register.php';
  * Add Theme Support for Project Thumbnails
  * */
 add_theme_support( 'post-thumbnails', array( 'project' ) );
-
-/**
- * Add block category for GFL Centers
- * */
-if ( function_exists( 'register_block_pattern_category' ) ) {
-	register_block_pattern_category(
-		'gfl-center',
-		array(
-			'label' => __( 'GFL Center', 'ke-pitchfork' ),
-			'description' => __( 'Pages for GFL Centers', 'ke-pitchfork' ),
-		)
-	);
-}
