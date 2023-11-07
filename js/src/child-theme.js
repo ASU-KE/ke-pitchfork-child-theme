0ae96d2d41008370941708f919ad729b314ce11d
Walter McConnell <Walter.McConnell@asu.edu>
2023-11-06 17:00:10 -0700

    Add Sticky/Unsticky JS
    Added the JS snipped (plucked from our main theme) that adds and removes the 'sticky' class to anchor menus when needed.

--- js/src/child-theme.js
+++ js/src/child-theme.js
@@ -1,1 +1,10 @@
 // Start your project's JS here.
+
+// Add/remove sticky menu class to anchor menus after a certain scroll distance.
+jQuery(window).scroll(function(){
+	var stickyMenu = jQuery('.uds-anchor-menu'),
+	scroll = jQuery(window).scrollTop();
+
+	if (scroll <= 500) stickyMenu.removeClass('sticky');
+	else stickyMenu.addClass('sticky');
+	});
