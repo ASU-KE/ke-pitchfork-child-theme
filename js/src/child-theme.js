3e05b236e554b84a0ccd6b78111365630f0ea6fd
Walter McConnell <Walter.McConnell@asu.edu>
2023-11-07 10:38:15 -0700

    Reduce scroll amount trigger
    Because these sites are using a lot of small heroes, the anchor menus would disappear for a bit before sticking. I reduced the amount of scrolling needed before the menu would stick.

--- js/src/child-theme.js
+++ js/src/child-theme.js
@@ -1,10 +1,13 @@
 // Start your project's JS here.
 
 // Add/remove sticky menu class to anchor menus after a certain scroll distance.
+jQuery(document).ready( function() {
 jQuery(window).scroll(function(){
 	var stickyMenu = jQuery('.uds-anchor-menu'),
 	scroll = jQuery(window).scrollTop();
 
-	if (scroll <= 500) stickyMenu.removeClass('sticky');
+  // Note: the use of several small heroes made me reduce the scroll threshold to 256px.
+	if (scroll <= 256) stickyMenu.removeClass('sticky');
 	else stickyMenu.addClass('sticky');
 	});
+})
